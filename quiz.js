2267e7ad7515795e4526d9423d1f36819cc9770f
Evelyn Alamba <eala56847@gmail.com>
2025-06-23 15:54:29 +0100

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,14 @@
+function checkAnswer() {
+
+  const correctAnswer = "4"
+  const userAnswer = document.querySelector("input[name='quiz']").value
+  if(correctAnswer === userAnswer)
+  {
+
+    document.querySelector("#feedback").textContent = "Correct! Well done."
+  }
+
+  else
+     document.querySelector("#feedback").textContent = "That's incorrect. Try again!"
+}
+document.getElementById("submit-answer").addEventListener('click', checkAnswer)
